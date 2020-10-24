bc683297d4bd0c663ace2fe45783df9e91ae5607
Wemerson Souto <wpsouto@gmail.com>
2020-10-24 00:26:40 -0300

    Start mobx

--- lib/app/core/stores/change_theme_store.dart
+++ lib/app/core/stores/change_theme_store.dart
@@ -1,4 +1,16 @@
-//MOB_X
-// class ChangeThemeStore = _ChangeThemeStoreBase with _$ChangeThemeStore;
-//
-// abstract class _ChangeThemeStoreBase with Store {}
+import 'package:flutter_arch/app/core/interfaces/local_storage_interface.dart';
+import 'package:flutter_arch/app/core/models/app_config_model.dart';
+import 'package:mobx/mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+part 'change_theme_store.g.dart';
+
+@Injectable()
+class ChangeThemeStore = _ChangeThemeStoreBase with _$ChangeThemeStore;
+
+abstract class _ChangeThemeStoreBase with Store {
+  // final ILocalStorage storage;
+  // final AppConfigModel config = AppConfigModel();
+
+
+}
